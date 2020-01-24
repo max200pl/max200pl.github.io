1200a719604bb87cc0e87832b5a396b4f020e429
max200pl <max2000pl@gmail.com>
2020-01-24 15:33:47 +0200

    change of principles scroll

--- Anveshan/js/common.js
+++ Anveshan/js/common.js
@@ -1,47 +1,52 @@
-$(function(){
-	let header = $("#header"),
-	   	 introH = $("#intro").innerHeight(),
-	   	 scrollOffset = $(window).scrollTop(),
-	   	 headerH = $("#header").innerHeight();
-	       checkScroll(scrollOffset);
+$(function() {
+  let header = $("#header"),
+    introH = $("#intro").outerHeight(),
+    scrollOffset = $(window).scrollTop(),
+    headerH = $("#header__inner").outerHeight();
+  checkScroll(scrollOffset);
 
-	      		
-	      $(window).on("scroll",function(){
-	      	scrollOffset = $(this).scrollTop();
-	      	scrollOffset = scrollOffset+headerH; 
-	      	checkScroll(scrollOffset);
-	      });
+  $(window).on("scroll", function() {
+    scrollOffset = $(this).scrollTop();
+    scrollOffset = scrollOffset;
+    checkScroll(scrollOffset);
+  });
 
-	      function checkScroll(scrollOffset){
-	      	let offsetIntro= introH - headerH;
-	      	if( scrollOffset >= offsetIntro){
-	      		header.addClass("fixed");
-	      	}else{
-	      		header.removeClass("fixed");
-	      	}
-	      }
+  function checkScroll(scrollOffset) {
+    let offsetIntro = introH - headerH;
+    if (scrollOffset >= offsetIntro) {
+      header.addClass("fixed");
+      $("#header__inner").addClass("bgColor");
+    } else {
+      header.removeClass("fixed");
+      $("#header__inner").removeClass("bgColor");
+    }
+  }
 
-	      $("[data-scroll]").on("click", function(event) {
-	      	event.preventDefault();
+  $("[data-scroll]").on("click", function(event) {
+    event.preventDefault();
 
-	      	let $this=$(this),
-	      		 blockId = $this.data('scroll'),
-	      		 blockOffset = ($(blockId).offset().top)-headerH; 
-	      		 console.log(blockOffset);
+    let $this = $(this),
+      blockId = $this.data("scroll"),
+      blockOffset = $(blockId).offset().top - headerH;
+    console.log(blockOffset);
 
-	     			 $("html, body").animate({
-	      		scrollTop: blockOffset}, 500);
-	      });
+    $("html, body").animate(
+      {
+        scrollTop: blockOffset
+      },
+      600
+    );
+  });
 
-	      $("#nav__toggle").on("click", function(event) {
-	      	event.preventDefault(); 
-	      	$(this).toggleClass("active");
-	      	$("#nav__list").toggleClass("show");
-	      });
+  $("#nav__toggle").on("click", function(event) {
+    event.preventDefault();
+    $(this).toggleClass("active");
+    $("#nav__list").toggleClass("show");
+  });
 
-	      $("#nav__menu").on("click", function(event) {
-	       	event.preventDefault(); 
-	      	$("#nav__toggle").toggleClass("active");
-	       	$("#nav__list").toggleClass("show");
-	      });
+  $("#nav__menu").on("click", function(event) {
+    event.preventDefault();
+    $("#nav__toggle").toggleClass("active");
+    $("#nav__list").toggleClass("show");
+  });
 });
