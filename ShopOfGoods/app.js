a164a1c279eeec0aa77cc0fc63d35bd8f031962c
max200pl <max2000pl@gmail.com>
2020-10-22 21:42:55 +0200

    add logic, find price cost for product, by click button "начать оформление"

--- ShopOfGoods/app.js
+++ ShopOfGoods/app.js
@@ -4,20 +4,25 @@
 document.addEventListener('click', function (event) {
      if (event.target.dataset.hide != undefined) { // .dataset.hide - дата атрибут с именем hide 
           event.target.style.display = 'none';
+          let parent = event.target.parentNode.parentNode // получение родителя 
+          let priceCost = parent.getElementsByClassName("price__cost")[0]
+          let cost = priceCost.innerText
+          console.log(cost);
      }
 })
-
 //===== Counter Product
 
 let buttonCountPlus = document.getElementById("trade-tuning__buttonCountPlus")
 
+//let attribute  = document.getAttribute('[data-item]')
+
 
 buttonCountPlus.addEventListener('click', function (event) {
-    
-     
+
+
 })
 
-// нужно повесить обработчик на id="item"
+// нужно повесить обработчик на 
 //item.addEventListener
 
 // при нажатии на данный блок считать class="price__cost"
