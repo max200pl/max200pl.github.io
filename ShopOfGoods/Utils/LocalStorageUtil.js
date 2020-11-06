4406878c690beb624c96b3dec4642097c680c7a9
max200pl <max2000pl@gmail.com>
2020-11-06 20:51:49 +0100

    writed two methods work push and get products to local Storage

--- ShopOfGoods/Utils/LocalStorageUtil.js
+++ ShopOfGoods/Utils/LocalStorageUtil.js
@@ -1,25 +1,37 @@
- class LocalStorageUtil{
-      constructor(){
-           // задаем новое свойство 
-           this.keyName = 'products'
-      }
-      // создаем новые два метода, для получения списка выбранных продуктов и добавления новых 
+class LocalStorageUtil {
+     constructor() {
+          this.keyName = 'products';// создаем ключ по которому будем обращаться 
+     }
+     // создаем новые два метода, для получения списка выбранных продуктов и добавления новых 
 
-      getProducts(){
-           // получаем элемент по ключу, если элемент присутствует получаем строку если нет то null 
-          const productsLocalStorage = localStorage.getItem(this.keyName)
-          if (productsLocalStorage ==! null) {
+     getProducts() {
+          // получаем элемент по ключу, если элемент присутствует получаем строку если нет то null 
+          const productsLocalStorage = localStorage.getItem(this.keyName);
+          if (productsLocalStorage !== null) {
                //если не null то распарим строку и переведем ее в массив 
-               return JSON.parse(productsLocalStorage)
+               return JSON.parse(productsLocalStorage);
           }
           return [];
-      }
-      putProducts(id){ // принимаем id товара с объекта Products
+     }
 
-      }
- }
+     putProducts(id) { // принимаем id товара с объекта Products
+          let products = this.getProducts();// получаем массив 
+          let pushProduct = false;// если значение false  то удалили если true то добавили 
+          // проверка повторяющихся элементов массива 
+          const index = products.indexOf(id);
 
- const localStorageUtil = new LocalStorageUtil();
+          if (index === -1) {
+               products.push(id);
+               pushProduct = true;// если новый элемент возводим флаг в true 
+          } else {
+               products.splice(index, 1) // удаляем повторяющийся элемент 
+          }
 
-const a =  localStorageUtil.getProducts();
-console.log(a);
+          localStorage.setItem(this.keyName, JSON.stringify(products)); // преобразуем из массива назад в строку 
+
+          return { pushProduct, products }
+     }
+}
+
+const localStorageUtil = new LocalStorageUtil();
+
