9fbe644d3f884f59a65f426a64e185693dc68280
max200pl <max2000pl@gmail.com>
2020-11-16 20:56:47 +0100

    realization logic work spinner

--- ShopOfGoods/index.js
+++ ShopOfGoods/index.js
@@ -5,13 +5,19 @@ function render(){
      productsPage.render()
 }
 
+spinnerPage.render()
+
 let CATALOG = [];
 //https://api.myjson.com/bins/esicc
 fetch('server/catalog.json')
-    .then(res => res.json())
-    .then(body => {
-        CATALOG = body;
-        render();
+    .then(res => res.json()) // получаем в формате json 
+    .then(body => {  // получем body 
+        CATALOG = body; // записываем в переменную CATALOG
+
+        setTimeout(() => { // для проверки корректной работы spinner
+            spinnerPage.handleClear() // после загрузки в CATALOG удаляем спиннер  
+            render(); // рисуем полученный результат 
+        }, 1000);
     })
     .catch(error => {
         console.log(error);
