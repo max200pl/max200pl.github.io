4f2713f99e95d40a4ea5fad9f74c45a66857a149
max200pl <max2000pl@gmail.com>
2021-02-09 11:57:40 +0200

    connect axios and creat gist (catalog.json)  autput in console

--- ShopOfGoods/src/index.js
+++ ShopOfGoods/src/index.js
@@ -1,3 +1,22 @@
+import axios from "axios";
+
+/**
+ ** 1) через метод .get() библиотеки axios получим данные с сервера 
+ ** 1.1) .get(url) -> вернет promise 
+ ** 2) присвоим переменной url по которому будем получать данные 
+ ** 3) выведем в консоль 
+*/
+const url = 'https://gist.githubusercontent.com/max200pl/a9d71924a75c18deed1292d13b42f263/raw/2baff97d55a48b2e58bebc64d45468927aeb54d3/catalog.json';    
+const API_KEY = '' // в случае необходимости 
+axios.get(url, {
+        params: { // объект дополнительных параметров для формирования ключа доступа 
+            apikey: API_KEY
+        }
+    })
+    .then(result => console.log(result.data))
+    .catch(error => console.log(console.error(error.message)))
+
+
 function render(){
     const productsStore = localStorageUtil.getCountProducts(); // получаем массив с элементами(карточками)
     headerPage.render(productsStore); // (count) изменяем содержимое корзины в зависимости от localStorage в статике с перезагрузкой страницы 
