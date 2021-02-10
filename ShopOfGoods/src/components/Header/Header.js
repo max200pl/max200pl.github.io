ff82a61369654c29404a5c1f2da2be37c2ea8e75
max200pl <max2000pl@gmail.com>
2021-02-10 11:37:07 +0200

    created structure for asynchronous request per server

--- ShopOfGoods/src/components/Header/Header.js
+++ ShopOfGoods/src/components/Header/Header.js
@@ -1,43 +1,43 @@
-class Header {
-     //* метод вызова модального окна Корзины
-     handleOpenShoppingPage(){
-          shoppingPage.render();
-     }
-     //*
+// class Header {
+//      //* метод вызова модального окна Корзины
+//      handleOpenShoppingPage(){
+//           shoppingPage.render();
+//      }
+//      //*
 
-     render(count) {
-          const html = `
-               <header class="header">
-                    <div class="container container--br-mb__15px">
-                         <div class="header__inner">
-                              <div class="header__logo">
-                                   DreamJob
-                              </div>
+//      render(count) {
+//           const html = `
+//                <header class="header">
+//                     <div class="container container--br-mb__15px">
+//                          <div class="header__inner">
+//                               <div class="header__logo">
+//                                    DreamJob
+//                               </div>
 
-                              <div class="menu">
-                                   <form action="/search/" target="_blank" class="menu-search">
-                                        <input class="menu-search__input" type="search" placeholder="Искать..." autofocus>
-                                   </form>
+//                               <div class="menu">
+//                                    <form action="/search/" target="_blank" class="menu-search">
+//                                         <input class="menu-search__input" type="search" placeholder="Искать..." autofocus>
+//                                    </form>
 
-                                   <div class="menu-basket" onClick = "headerPage.handleOpenShoppingPage();">
-                                        <span id="total-cart-count" class="menu-basket__badge"> ${count}</span>
-                                        <span id="amount" class="menu-basket__amount">4800р</span>
-                                   </div>
+//                                    <div class="menu-basket" onClick = "headerPage.handleOpenShoppingPage();">
+//                                         <span id="total-cart-count" class="menu-basket__badge"> ${count}</span>
+//                                         <span id="amount" class="menu-basket__amount">4800р</span>
+//                                    </div>
 
-                                   <div class="menu-leng">
-                                        <span id="choice" class="menu-leng__choice">Eng</span>
-                                   </div>
-                              </div>
-                         </div>
-                    </div>
-               </header>
-          `;
-          ROOT_HEADER.innerHTML = html;
-     }
-}
+//                                    <div class="menu-leng">
+//                                         <span id="choice" class="menu-leng__choice">Eng</span>
+//                                    </div>
+//                               </div>
+//                          </div>
+//                     </div>
+//                </header>
+//           `;
+//           ROOT_HEADER.innerHTML = html;
+//      }
+// }
 
-const headerPage = new Header();
+// const headerPage = new Header();
 
-const productsStore = localStorageUtil.getCountProducts(); // получаем массив с элементами(карточками)
-//* productsStore.length // получаем количество элементов массива 
-headerPage.render(productsStore); // (count) изменяем содержимое корзины в зависимости от localStorage в статике с перезагрузкой страницы 
+// const productsStore = localStorageUtil.getCountProducts(); // получаем массив с элементами(карточками)
+// //* productsStore.length // получаем количество элементов массива 
+// headerPage.render(productsStore); // (count) изменяем содержимое корзины в зависимости от localStorage в статике с перезагрузкой страницы 
--- ShopOfGoods/src/index.js
+++ ShopOfGoods/src/index.js
@@ -1,25 +1,62 @@
 import axios from "axios";
+import "regenerator-runtime/runtime.js";
+const url = 'https://gist.githubusercontent.com/max200pl/a9d71924a75c18deed1292d13b42f263/raw/2baff97d55a48b2e58bebc64d45468927aeb54d3/catalog.json';
+const API_KEY = '' // в случае необходимости 
 
 /**
- ** 1) через метод .get() библиотеки axios получим данные с сервера 
- ** 1.1) .get(url) -> вернет promise 
- ** 2) присвоим переменной url по которому будем получать данные 
+ ** 1) 
+ ** 1.1) 
+ ** 2) 
  ** 3) выведем в консоль 
 */
-const url = 'https://gist.githubusercontent.com/max200pl/a9d71924a75c18deed1292d13b42f263/raw/2baff97d55a48b2e58bebc64d45468927aeb54d3/catalog.json';    
-const API_KEY = '' // в случае необходимости 
-axios.get(url, {
-        params: { // объект дополнительных параметров для формирования ключа доступа 
-            apikey: API_KEY
-        }
-    })
-    .then(result => console.log(result.data))
-    .catch(error => console.log(console.error(error.message)))
 
+/**
+ ** -- создаем класс который будет возвращать данные с сервера 
+ ** -- в class GetDataApi, прописываем конструкцию try{} catch{} для обработки ошибок 
+ ** -- в try{} создаем переменную для данных с сервера 
+ ** -- прописываем ключевое слово await для ожидания получения данных
+ ** -- возвращаем полученные данные через метод .get() 
+        --через метод .get() библиотеки axios получим данные с сервера 
+        -- .get(url) -> вернет promise 
+        --присвоим переменной url c git - gist по которому будем получать данные 
+ ** -- создаем экземпляр класса через new
+ ** -- вызываем метод getDataApi и передаем URL сервера 
+ ** -- настраиваем асинхронные функции для axios
+ **     6.1 импортируем regenerator-runtime 
+ ** -- создаем самовзрывающуюся асинхронную функцию (async()=>{})()
+ ** -- ожидаем данные с сервера через конструкцию async 
+ */
+
+class GetDataApi {
+    async getData(url) {
+        try {
+            const response = await axios.get(url, {
+                params: { // объект дополнительных параметров для формирования ключа доступа 
+                    apikey: API_KEY
+                }
+            })
+            return response.data
+        } catch (error) {
+            console.log(error.message);
+            return false;
+        }
+    }
+}
+
+const getDataApi = new GetDataApi();
+
+
+(async () => { // await response server
+    const data = getDataApi.getData(url); // return promise 
+    console.log(data);
+})();
+
+
+/*
 
 function render(){
     const productsStore = localStorageUtil.getCountProducts(); // получаем массив с элементами(карточками)
-    headerPage.render(productsStore); // (count) изменяем содержимое корзины в зависимости от localStorage в статике с перезагрузкой страницы 
+    headerPage.render(productsStore); // (count) изменяем содержимое корзины в зависимости от localStorage в статике с перезагрузкой страницы
     productsPage.render()
 }
 
@@ -28,16 +65,16 @@ spinnerPage.render()
 let CATALOG = [];
 //https://api.myjson.com/bins/esicc
 fetch('server/catalog.json')
-    .then(res => res.json()) // получаем в формате json 
-    .then(body => {  // получем body 
+    .then(res => res.json()) // получаем в формате json
+    .then(body => {  // получем body
         CATALOG = body; // записываем в переменную CATALOG
 
         setTimeout(() => { // для проверки корректной работы spinner
-            spinnerPage.handleClear() // после загрузки в CATALOG удаляем спиннер  
-            render(); // рисуем полученный результат 
+            spinnerPage.handleClear() // после загрузки в CATALOG удаляем спиннер
+            render(); // рисуем полученный результат
         }, 1000);
     })
     .catch(()=> {
-        spinnerPage.handleClear() // удаляем спиннер 
+        spinnerPage.handleClear() // удаляем спиннер
         errorPage.render();
-    })
+    }) */
